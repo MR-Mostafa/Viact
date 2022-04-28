2a088b55ffa8be487921d86b7983ed00559924f0
Mostafa Rahmati <m.rahmati70@hotmail.com>
2022-04-29 02:49:19 +0430

    eslint & typescript config

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+	settings: {
+		'import/resolver': {
+			node: {
+				paths: ['src'],
+				extensions: ['.js', '.jsx', '.ts', '.tsx'],
+			},
+		},
+	},
+	plugins: ['github', 'regexp'],
+	extends: [
+		'eslint:recommended',
+		'plugin:listeners/recommended',
+		'plugin:github/recommended',
+		'@fullstacksjs',
+		'@fullstacksjs/eslint-config/typecheck',
+		'plugin:regexp/recommended',
+		'plugin:security/recommended',
+	],
+	parserOptions: {
+		project: './tsconfig.eslint.json',
+	},
+	rules: {
+		'prettier/prettier': ['error', {}, { usePrettierrc: true, singleQuote: true }],
+	},
+};

182f2913646a4dd29bf0064be4764f42c4599978
Mostafa Rahmati <m.rahmati70@hotmail.com>
2022-04-29 17:26:20 +0430

    update: config proxy

--- vite.config.ts
+++ vite.config.ts
@@ -46,5 +46,19 @@ export default defineConfig({
 	envPrefix: 'GB_',
 	server: {
 		https: true,
+		proxy: {
+			/**
+			 * When we use proxy API requests, we can get data from an external website that throws an error by default (if we don't use a proxy)
+			 * https://localhost:3000/api/**  ->  https://jsonplaceholder.typicode.com/**
+			 *
+			 * @example
+			 * 	https://localhost:3000/api/todos/1  ->  https://jsonplaceholder.typicode.com/todos/1
+			 **/
+			'/api/': {
+				target: 'https://jsonplaceholder.typicode.com/',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ''),
+			},
+		},
 	},
 });

c3c65611723c6577bab3940864f8de3232cf3209
Mostafa Rahmati <m.rahmati70@hotmail.com>
2022-04-29 15:20:13 +0430

    update: add some alias dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	plugins: [react()],
+	cacheDir: './.vite-cache',
+	resolve: {
+		alias: {
+			'@src': resolve(__dirname, 'src'),
+			'@components': resolve(__dirname, 'src', 'components'),
+			'@hooks': resolve(__dirname, 'src', 'hooks'),
+			'@api': resolve(__dirname, 'src', 'api'),
+			'@store': resolve(__dirname, 'src', 'store'),
+			'@images': resolve(__dirname, 'src', 'assets', 'images'),
+			'@styles': resolve(__dirname, 'src', 'assets', 'styles'),
+			'@fonts': resolve(__dirname, 'src', 'assets', 'fonts'),
+			'@utils': resolve(__dirname, 'src', 'utils'),
+			'@pages': resolve(__dirname, 'src', 'pages'),
+			'@constants': resolve(__dirname, 'src', 'constants'),
+			'@containers': resolve(__dirname, 'src', 'containers'),
+			'@types': resolve(__dirname, 'src', 'types'),
+		},
+	},
+});

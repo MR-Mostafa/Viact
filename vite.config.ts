0995fe1a01b690f4268e46faa0a02ea4946309b0
Mostafa Rahmati <m.rahmati70@hotmail.com>
2022-04-29 16:00:49 +0430

    update: support https

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		mkcert({
+			source: 'coding',
+		}),
+	],
 	cacheDir: './.vite-cache',
 	resolve: {
 		alias: {
@@ -25,4 +31,7 @@ export default defineConfig({
 	},
 	envDir: resolve(__dirname, 'src', 'env'),
 	envPrefix: 'GB_',
+	server: {
+		https: true,
+	},
 });

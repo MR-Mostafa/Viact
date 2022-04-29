a53ff5174324581859dc952dd92df3b8975e1303
Mostafa Rahmati <m.rahmati70@hotmail.com>
2022-04-29 18:10:02 +0430

    update: importOrder & chage @ to @@ for local alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import mkcert from 'vite-plugin-mkcert';
+
 import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
+import mkcert from 'vite-plugin-mkcert';
 
 const shouldAnalyze = process.env.ANALYZE;
 
@@ -27,19 +28,19 @@ export default defineConfig({
 	cacheDir: './.vite-cache',
 	resolve: {
 		alias: {
-			'@src': resolve(__dirname, 'src'),
-			'@components': resolve(__dirname, 'src', 'components'),
-			'@hooks': resolve(__dirname, 'src', 'hooks'),
-			'@api': resolve(__dirname, 'src', 'api'),
-			'@store': resolve(__dirname, 'src', 'store'),
-			'@images': resolve(__dirname, 'src', 'assets', 'images'),
-			'@styles': resolve(__dirname, 'src', 'assets', 'styles'),
-			'@fonts': resolve(__dirname, 'src', 'assets', 'fonts'),
-			'@utils': resolve(__dirname, 'src', 'utils'),
-			'@pages': resolve(__dirname, 'src', 'pages'),
-			'@constants': resolve(__dirname, 'src', 'constants'),
-			'@containers': resolve(__dirname, 'src', 'containers'),
-			'@types': resolve(__dirname, 'src', 'types'),
+			'@@src': resolve(__dirname, 'src'),
+			'@@components': resolve(__dirname, 'src', 'components'),
+			'@@hooks': resolve(__dirname, 'src', 'hooks'),
+			'@@api': resolve(__dirname, 'src', 'api'),
+			'@@store': resolve(__dirname, 'src', 'store'),
+			'@@images': resolve(__dirname, 'src', 'assets', 'images'),
+			'@@styles': resolve(__dirname, 'src', 'assets', 'styles'),
+			'@@fonts': resolve(__dirname, 'src', 'assets', 'fonts'),
+			'@@utils': resolve(__dirname, 'src', 'utils'),
+			'@@pages': resolve(__dirname, 'src', 'pages'),
+			'@@constants': resolve(__dirname, 'src', 'constants'),
+			'@@containers': resolve(__dirname, 'src', 'containers'),
+			'@@types': resolve(__dirname, 'src', 'types'),
 		},
 	},
 	envDir: resolve(__dirname, 'src', 'env'),

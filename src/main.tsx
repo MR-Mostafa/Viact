ad9351348c5c84db3fe049c0b42a0a0c59d930a2
Mostafa Rahmati <M.Rahmati70@hotmail.com>
2022-05-08 18:27:19 +0430

    add style to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import 'uno.css';
+
+import '~styles/styles.rtl.scss';
+import '~styles/styles.scss';
+
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode>
+	</React.StrictMode>,
 );
